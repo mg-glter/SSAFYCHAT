dc557f4dd34962567d2757a3122d307e6c5297dc
kdw3238 <kdw3238@naver.com>
2023-01-25 17:36:03 +0900

    [Feat] CardList Component : 카드리스트 컴포넌트, css 추가

--- front/src/components/common/CardList.tsx
+++ front/src/components/common/CardList.tsx
@@ -1,1 +1,12 @@
-export {}
+import "../../styles/components/common/card-list.css"
+function CardList(props : any){
+    return (
+    <div className="card_list_container">
+        <div className="inner_container">
+            테스트
+        </div>
+    </div>
+    )
+}
+
+export default CardList;
