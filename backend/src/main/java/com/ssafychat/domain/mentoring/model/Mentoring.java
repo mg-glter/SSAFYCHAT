eb5655cf9c6678e787ffc4c8e5606308782f1fbf
sairo <from415@kakao.com>
2023-01-26 15:31:04 +0900

    [Feat] 멘토링 엔티티 작성 : apply_mentoring, mentoring, cancel_mentoring, complete_mentoring, report, mentoring_date 엔티티 작성. 관계 설정 필요

--- backend/src/main/java/com/ssafychat/domain/mentoring/model/Mentoring.java
+++ backend/src/main/java/com/ssafychat/domain/mentoring/model/Mentoring.java
@@ -16,4 +16,14 @@ public class Mentoring {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "mentoring_id")
     private int mentoringId;
+    @Column(name = "mentee_uid", nullable = false) // 외래키
+    private int menteeUid;
+    @Column(name = "mentor_uid", nullable = false) // 외래키
+    private int mentorUid;
+    @Column
+    private String time; // 1. 형식? 2. default가 null로 되어 있는데 맞나요?
+    @Column(nullable = false)
+    private String job;
+    @Column(nullable = false)
+    private String company;
 }
