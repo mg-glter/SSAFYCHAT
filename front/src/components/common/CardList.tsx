11c69f700c26682cce10df9f11cdaaad39d5a7bc
kdw3238 <kdw3238@naver.com>
2023-01-26 17:08:44 +0900

    [Feat] ReservedList Component : 리저브드리스트 컴포넌트 추가

--- front/src/components/common/CardList.tsx
+++ front/src/components/common/CardList.tsx
@@ -1,9 +1,51 @@
-import "../../styles/components/common/card-list.css"
+import "../../styles/components/common/card-list.css";
+
+function Card(props : any){
+    return(
+        <div className="card_outer">
+            <div className="card">
+
+          </div>
+        </div>
+    )
+}
+
 function CardList(props : any){
     return (
+    // 카드리스트 전체를 감싸는 컨테이너
     <div className="card_list_container">
-        <div className="inner_container">
-            테스트
+
+        {/* 좌 화살표 */}
+        <div className="card_list_arrow">
+            <div className="arrow_button">
+                <div className="arrow">&lt;</div>
+            </div>
+        </div>
+
+        {/* 카드리스트와 헤더를 감싸는 컨테이너 */}
+        <div className="card_list_inner_container">
+            {/* 헤더 */}
+           <div className="card_list_header">
+                <div className="header_text">
+                    무엇의 목록
+                </div>
+           </div>
+
+            {/* 카드리스트 */}
+            <div className="card_container">
+                {/* 카드 */}
+                <Card></Card>
+                <Card></Card>                
+            </div>
+        </div>
+
+        {/* 우측 화살표 */}
+        <div className="card_list_arrow">
+        <div className="arrow_button">
+            <div className="arrow">
+                &gt;
+            </div>
+        </div>
         </div>
     </div>
     )
