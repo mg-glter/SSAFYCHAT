8c9aa00adff8f45efb45a3321595725cf4d4a01f
박정희 <wjdgmll77@naver.com>
2023-01-26 15:33:14 +0900

    [Feat] 백엔드 컨트롤러 : rest api명세서 기반 맵핑설정

--- backend/src/main/java/com/ssafychat/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/ssafychat/domain/member/controller/MemberController.java
@@ -1,4 +1,41 @@
 package com.ssafychat.domain.member.controller;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin("*")
 public class MemberController {
+    @GetMapping("/")
+    public ResponseEntity<?> aliveCheck() {
+        return new ResponseEntity<String>("Alive", HttpStatus.OK);
+    }
+
+    @GetMapping("/mentoring-infos")
+    public ResponseEntity<?> mentoringInfo() {
+        return new ResponseEntity<String>("mentoring-infos", HttpStatus.OK);
+    }
+    @GetMapping("/regist")
+    public ResponseEntity<?> registUser() {
+        return new ResponseEntity<String>("regist", HttpStatus.OK);
+    }
+    @GetMapping("/registinfo")//생각해볼 것 소셜로그인 매커니즘에 따르면 rest api가 필요한가?
+    public ResponseEntity<?> socialRegist() {
+        return new ResponseEntity<String>("registinfo", HttpStatus.OK);
+    }
+    @GetMapping("/login")
+    public ResponseEntity<?> nomalLogin() {
+        return new ResponseEntity<String>("login", HttpStatus.OK);
+    }
+    @GetMapping("/user-info")
+    public ResponseEntity<?> userInfo() {
+        return new ResponseEntity<String>("user-info", HttpStatus.OK);
+    }
+    @GetMapping("/login/{social}")
+    public ResponseEntity<?> socialLogin(@PathVariable("social") String social) {
+        return new ResponseEntity<String>("social:"+social, HttpStatus.OK);
+    }
 }
--- backend/src/main/java/com/ssafychat/domain/mentoring/controller/MentoringController.java
+++ backend/src/main/java/com/ssafychat/domain/mentoring/controller/MentoringController.java
@@ -3,6 +3,8 @@ package com.ssafychat.domain.mentoring.controller;
 import com.ssafychat.domain.mentoring.model.Mentoring;
 import com.ssafychat.domain.mentoring.service.MentoringServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,9 +16,55 @@ public class MentoringController {
     @Autowired
     private MentoringServiceImpl mentoringService;
 
+    @GetMapping("/")
+    public ResponseEntity<?> aliveCheck() {
+        return new ResponseEntity<String>("Alive", HttpStatus.OK);
+    }
     @GetMapping
     public List<Mentoring> findMentoring() {
         return mentoringService.findMentoring();
     }
+
+    @GetMapping("/weekly-rankers")
+    public ResponseEntity<?> weeklyRankers() {
+        return new ResponseEntity<String>("weekly-rankers", HttpStatus.OK);
+    }
+    @GetMapping("/main-info")
+    public ResponseEntity<?> mainInfo() {
+        return new ResponseEntity<String>("main-info", HttpStatus.OK);
+    }
+    @GetMapping("/job/{job}")
+    public ResponseEntity<?> jobSeach(@PathVariable("job") String job) {
+        return new ResponseEntity<String>("job:"+job, HttpStatus.OK);
+    }
+    @PostMapping("/apply")
+    public ResponseEntity<?> applayMentoring() {
+        return new ResponseEntity<String>("apply", HttpStatus.OK);
+    }
+    @GetMapping("/reservation")
+    public ResponseEntity<?> reservationMentoring() {
+        return new ResponseEntity<String>("reservation", HttpStatus.OK);
+    }
+    @DeleteMapping("/cancel/appointment")
+    public ResponseEntity<?> appointmentCancel() {
+        return new ResponseEntity<String>("cancel/appointment", HttpStatus.OK);
+    }
+    @DeleteMapping("/cancel/reservation")
+    public ResponseEntity<?> reservationCancel() {
+        return new ResponseEntity<String>("cancel/reservation", HttpStatus.OK);
+    }
+    @GetMapping("/appointment")
+    public ResponseEntity<?> appointmentSearch() {
+        return new ResponseEntity<String>("appointment", HttpStatus.OK);
+    }
+    @PostMapping("/appointment")
+    public ResponseEntity<?> appointmentAccept() {
+        return new ResponseEntity<String>("appointment", HttpStatus.OK);
+    }
+    @GetMapping("/review")
+    public ResponseEntity<?> reviewRollingpaper() {
+        return new ResponseEntity<String>("review", HttpStatus.OK);
+    }
+
 
 }
