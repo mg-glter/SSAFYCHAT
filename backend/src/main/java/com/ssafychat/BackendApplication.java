e214e15adb692adcfcb072f74b5816a46768be81
sairo <from415@kakao.com>
2023-01-26 10:07:28 +0900

    [Refactor] 디렉토리 구조 변경 : com.ssafychat.backend -> com.ssafychat

--- backend/src/main/java/com/ssafychat/BackendApplication.java
+++ backend/src/main/java/com/ssafychat/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.ssafychat.backend;
+package com.ssafychat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/ssafychat/BackendApplicationTests.java
+++ backend/src/test/java/com/ssafychat/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ssafychat.backend;
+package com.ssafychat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
