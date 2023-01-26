5c686bb9977f4b173be2f9f7f39555b36eec2d09
socm9145 <socm9145@naver.com>
2023-01-26 17:02:59 +0900

    [Feat] layout 작성 : 메인 페이지 layout 적용, Header css 변경

--- front/src/layout/Footer.tsx
+++ front/src/layout/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer (){
-    return(
+    return (
         <div>
-            This is footer.
+           
         </div>
     )
 }
--- front/src/pages/MainPage.tsx
+++ front/src/pages/MainPage.tsx
@@ -1,1 +1,14 @@
-export {}
+import Header from "../layout/Header"
+import Footer from "../layout/Footer"
+import MainContainer from "../components/MainContainer"
+
+function MainPage (){
+    return (
+        <div>
+            <Header></Header>
+            <MainContainer></MainContainer>            
+            <Footer></Footer>
+        </div>
+    )
+}
+export default MainPage
