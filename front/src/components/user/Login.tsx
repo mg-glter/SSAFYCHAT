2f9a999b44835c286a64fc4304a8773c8ffb2928
sunmoon1030 <sunmoon1030@gmail.com>
2023-01-27 16:59:00 +0900

    [Feat] 로그인 : 로그인 HTML설계, 소셜 로그인 사진 추가

--- front/src/components/user/Login.tsx
+++ front/src/components/user/Login.tsx
@@ -1,3 +1,8 @@
+function Icon(props: any){
+    return(
+        <div></div>
+    )
+}
 function Text(props: any){
     switch (props.item){
         case 'id':
@@ -13,7 +18,7 @@ function TextBox(props : any){
     return(
         <div className="text_box">
             <div className="text_icon">
-                
+                <Icon item={props.item}></Icon>
             </div>
             <div className="text_text">
                 <Text item={props.item}></Text>
@@ -23,12 +28,49 @@ function TextBox(props : any){
 }
 
 function Login(props: any){
+    const imgUrlGithub = "/img/Github.png"
+    const imgUrlGoogle = "/img/Google.png"
+    const imgUrlkakao = "/img/kakao.png"
     return(
         <div className="login_contain">
-            <form className="login_text_box">
-                <TextBox item="id"></TextBox>
-                <TextBox item="pwd"></TextBox>
-            </form>
+            <div className="login_logo_name">SSAFY CHAT</div>
+            <div>
+                {/* 로그인 입력 박스 두개 */}
+                <form className="login_text_box">
+                    <div>
+                        <TextBox item="id"></TextBox>
+                        <TextBox item="pwd"></TextBox>
+                    </div>
+                    {/* 아이디 기억 */}
+                    <div>
+                        <input type="checkbox" name="save_id" id="save_id" />
+                        Remember Me
+                    </div>
+                    {/* 아이디 비밀번호 찾기 */}
+                    <div><a href="#">Forgot ID or Password?</a></div>
+                    {/* 로그인 버튼 */}
+                    <input type="submit" value="Sign In" />
+                </form>
+            </div>
+            <hr />
+            {/* 회원가입 버튼 */}
+            <div>
+                <button type="submit">Sing Up</button>
+            </div>
+            <div className="sns_login_contain">
+                <div className="github">
+                    <img src={imgUrlGithub} alt="Github" />
+                </div>
+                <div className="kakao">
+                    <img src={imgUrlGoogle} alt="Google" />
+                </div>
+                <div className="google">
+                    <img src={imgUrlkakao} alt="kakao" />
+                </div>
+            </div>
+            <div className="login_footer">
+                <p className="footer_text">ⓣTeam BlueBerryPie. All rights reserved.</p>
+            </div>
         </div>
     )
 }
