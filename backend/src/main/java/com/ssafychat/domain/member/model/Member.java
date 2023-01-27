05f0d2049fcee136fd625666b0ad947ca8ac10c9
신희수 <star_of_arabia@nate.com>
2023-01-27 14:56:26 +0900

    Revert "[Feat] 엔티티 수정 : Member 엔티티 설정"
    This reverts commit 6696741010001aadd827c232b3554d35f2ddef37.

--- backend/src/main/java/com/ssafychat/domain/member/model/Member.java
+++ backend/src/main/java/com/ssafychat/domain/member/model/Member.java
@@ -1,35 +1,4 @@
 package com.ssafychat.domain.member.model;
 
-import lombok.*;
-
-import javax.persistence.*;
-
-@Getter
-@ToString
-@AllArgsConstructor
-@RequiredArgsConstructor
-@Builder
-@Entity
 public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int userId;
-
-    private String job;
-    private String belong;
-
-    @Column(nullable = false)
-    private String role;
-    private int totalScore;
-    private String email;
-    private String password;
-    private String social;
-
-    @Column(nullable = false)
-    private int studentNumber;
-    private String refreshToken;
-
-    @Column(nullable = false)
-    private String name;
-
 }
