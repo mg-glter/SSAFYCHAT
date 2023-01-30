7b6dba60bd256afa7c23d8452341ffd1f52dbcbc
sunmoon1030 <sunmoon1030@gmail.com>
2023-01-30 17:43:17 +0900

    [Feat] 라우터 : 라우터 도안

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
+import {Route, Routes} from 'react-router-dom';
 import './App.css';
+import Join from './components/user/Join';
+import Login from './components/user/Login';
 import MainPage from './pages/MainPage';
-// import Header from './layout/Header';
-// import Banner from './components/common/Banner';
-// import ReservationPage from './pages/ReservationPage';
+
 function App() {
   return (
     <div className="App">
-      <MainPage></MainPage>
-      {/* <Route> <Route> */}
-      {/* <Header></Header> */}
-      {/* <Banner name="마이페이지" imgName="banner1"></Banner> */}
-      {/* <ReservationPage></ReservationPage> */}
-
+      <Routes>
+        <Route path="/" element={<Login></Login>}></Route>
+        <Route path='/index' element={<MainPage></MainPage>}></Route>
+        <Route path='/join' element={<Join></Join>}></Route>
+      </Routes>
     </div>
   );
 }
