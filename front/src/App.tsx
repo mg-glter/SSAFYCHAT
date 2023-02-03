3932ffd4cae9ab09a618f30c68589be6c0586d5e
socm9145 <socm9145@naver.com>
2023-02-03 15:12:55 +0900

    [Feat] 신청 모달 생성 : 모달 기능 추가

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Join from './components/user/Join';
 import Login from './components/user/Login';
 import VideoConferenceContainer from './container/VideoConferenceContainer';
+import ApplyingPage from './pages/ApplyingPage';
 import MainPage from './pages/MainPage';
 import MyPage from './pages/MyPage';
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Login></Login>}></Route>
         <Route path='/index' element={<MainPage></MainPage>}></Route>
         <Route path='/mypage' element={<MyPage></MyPage>}></Route>
+        <Route path='/apply' element={<ApplyingPage></ApplyingPage>}></Route>
         <Route path='/meeting' element={<VideoConferenceContainer></VideoConferenceContainer>}></Route>
         <Route path='/join' element={<Join></Join>}></Route>
       </Routes>
