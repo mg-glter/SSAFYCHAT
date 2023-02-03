3680fe7854aa0ca9aea1f10d46bc9562d7339b7c
kdw3238 <kdw3238@naver.com>
2023-02-03 16:37:17 +0900

    [Fix] App : 라우터 구조 설계

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,21 +1,15 @@
 import {Route, Routes} from 'react-router-dom';
 import './App.css';
-import Join from './components/user/Join';
-import Login from './components/user/Login';
-import VideoConferenceContainer from './container/VideoConferenceContainer';
 import MainPage from './pages/MainPage';
-import MyPage from './pages/MyPage';
-import ReservationContainer from './container/ReservationContainer';
 import BannerPage from './pages/BannerPage';
+import UserPage from './pages/UserPage';
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Login></Login>}></Route>
-        <Route path='/index' element={<MainPage></MainPage>}></Route>
-        <Route path='/join' element={<Join></Join>}></Route>
+        <Route path="/user/*" element={<UserPage></UserPage>}></Route>
+        <Route path='/*' element={<MainPage></MainPage>}></Route>
         <Route path='/banner/*' element={<BannerPage></BannerPage>}></Route>
-        <Route path='/meeting' element={<VideoConferenceContainer></VideoConferenceContainer>}></Route>
       </Routes>
       
       {/* <ReservationContainer></ReservationContainer> */}
