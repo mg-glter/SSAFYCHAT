b8bb5028e3c9bd93790e7d3c80c5ef8f618bf716
박정희 <wjdgmll77@naver.com>
2023-02-03 10:20:24 +0900

    [Feat] 멘토링방 : socket.io 서버변경

--- backend_chatting/src/server.js
+++ backend_chatting/src/server.js
@@ -1,14 +1,30 @@
 import http from "http";
-import WebSocket from "ws"; //ws 추가
+import { Server } from "socket.io"; // npm i socket.io
 
-//ws 통신 대기
-function handleConnection(socket) {
-    console.log("good");
-}
 const handleListen = () => console.log(`Listening on http://localhost:8000`);
 
-const server = http.createServer(); 
-const wss = new WebSocket.Server({server});
-wss.on("connection", handleConnection);
+const httpServer = http.createServer(); 
+const wsServer = new Server(httpServer);
+wsServer.on("connection", (socket) => {
 
-server.listen(8000,handleListen);
+    socket.on("join_room", (roomName) => { //answer단계에서는 done 생략
+        socket.join(roomName);
+        socket.to(roomName).emit("welcome");
+    });
+
+    //send단계
+    socket.on("offer", (offer, roomName) => {
+        socket.to(roomName).emit("offer", offer);
+    });
+    //answer단계
+    socket.on("answer", (answer, roomName) => {
+        socket.to(roomName).emit("answer", answer);
+    });
+
+    //ice candidate WebRTC에서 필요한 프로토콜 연결단계
+    socket.on("ice", (ice, roomName) => {
+        socket.to(roomName).emit("ice", ice);
+      });
+});
+
+httpServer.listen(8000,handleListen);
