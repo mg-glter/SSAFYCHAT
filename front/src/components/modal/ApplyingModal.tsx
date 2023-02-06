918cd1d948c9f88bf53eb591e2b712fdd394044d
socm9145 <socm9145@naver.com>
2023-02-06 13:34:53 +0900

    [Feat] 신청 모달 페이지 : 내부 구성요소 작성

--- front/src/components/modal/ApplyingModal.tsx
+++ front/src/components/modal/ApplyingModal.tsx
@@ -7,12 +7,34 @@ function ApplyingModal (props:any){
     }
 
     return (
-        <div className="Modal" onClick={closeModal}>
-            <div className="modalBody" onClick={(e) => e.stopPropagation()}>
-                <button id="modalCloseBtn" onClick={closeModal}>
-                ✖
-                </button>
-                {props.children}
+        <div className="modal" onClick={closeModal}>
+            <div className="modal_body" onClick={(e) => e.stopPropagation()}>
+                <div className='modal_header'>
+                    <button id="modal_close_btn" onClick={closeModal}>
+                    ✖
+                    </button>
+                </div>
+                <div className='modal_content'>
+                    <div className='modal_content_calendar_box'>
+                        <div className='modal_content_calendar'></div>
+                    </div>
+                    <div className='modal_content_select'>
+                        <div className='modal_content_selected_date'>
+                            <div className='modal_content_time_btn_clicked'>
+                                <div className='modal_content_time_text'>오전</div>
+                            </div>
+                            <div className='modal_content_time_btn'>
+                                <div className="modal_content_time_text">오후</div>
+                            </div>
+                            <div className='modal_content_day'>10</div>
+                            <div className='modal_content_month_year'>May</div>
+                            <div className='modal_content_month_year'>2023</div>
+                        </div>
+                        <div className='modal_content_selected_time'></div>
+                        <div className='modal_content_selected_times'></div>
+                        <div className='modal_content_selected_belong_job'></div>
+                    </div>
+                </div>
             </div>
         </div>
     )
