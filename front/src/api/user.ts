2c5745447a687065749f484111e1d8157460805c
sunmoon1030 <sunmoon1030@gmail.com>
2023-02-07 14:49:49 +0900

    [Fix] api : any 타입으로 대체

--- front/src/api/user.ts
+++ front/src/api/user.ts
@@ -2,7 +2,7 @@ import { localServer } from "./http";
 
 const api = localServer();
 
-async function login(user, success, fail) {
+async function login(user:any, success:any, fail:any) {
     await api.post('/login', JSON.stringify(user)).then(success).catch(fail);
 }
 
