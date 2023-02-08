5a4e64718d8ff38109154dbf441edf1e00c6c70f
sunmoon1030 <sunmoon1030@gmail.com>
2023-02-08 10:15:16 +0900

    [Feat] api : axios 방식 수정

--- front/src/api/http.ts
+++ front/src/api/http.ts
@@ -3,20 +3,13 @@ import axios, { AxiosRequestConfig } from "axios";
 // axios 객체 생성
 // localServer 통신
 function localServer() {
-  // const axiosConfig: AxiosRequestConfig = {
-  //   baseURL: "https://ssafychat.shop/api",
-  //   headers: {
-  //     "Content-Type": "application/json;charset=utf-8",
-  //   },
-  // }
-  // const instance = axios.create(axiosConfig);
-  // console.log(instance);
-  const instance = axios.create({
+  const axiosConfig: AxiosRequestConfig = {
     baseURL: "https://ssafychat.shop/api",
     headers: {
       "Content-Type": "application/json;charset=utf-8",
     },
-  })
+  }
+  const instance = axios.create(axiosConfig);
   return instance;
 }
 
