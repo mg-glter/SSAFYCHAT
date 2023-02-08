d00b44915951dfefcd1dc605ccba72dc5dbb0b11
sunmoon1030 <sunmoon1030@gmail.com>
2023-02-08 10:07:51 +0900

    [Feat] 로그인 : 로그인 기능 구현

--- front/src/api/http.ts
+++ front/src/api/http.ts
@@ -1,14 +1,22 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 // axios 객체 생성
 // localServer 통신
 function localServer() {
+  // const axiosConfig: AxiosRequestConfig = {
+  //   baseURL: "https://ssafychat.shop/api",
+  //   headers: {
+  //     "Content-Type": "application/json;charset=utf-8",
+  //   },
+  // }
+  // const instance = axios.create(axiosConfig);
+  // console.log(instance);
   const instance = axios.create({
     baseURL: "https://ssafychat.shop/api",
     headers: {
       "Content-Type": "application/json;charset=utf-8",
     },
-  });
+  })
   return instance;
 }
 
