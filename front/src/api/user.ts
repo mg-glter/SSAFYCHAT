0c89a20ee05e31d5ad523992a3b9cc80eee415a3
sunmoon1030 <sunmoon1030@gmail.com>
2023-02-08 12:38:52 +0900

    [Feat] 로그인 : Store 추가

--- front/src/api/user.ts
+++ front/src/api/user.ts
@@ -6,4 +6,8 @@ async function login(user:any, success:any, fail:any) {
     await api.post('/login', JSON.stringify(user)).then(success).catch(fail);
 }
 
-export {login};
+async function logout(userId: string, success:any, fail:any) {
+    await api.delete(`/logout/${userId}`).then(success).catch(fail);
+}
+
+export {login, logout};
--- front/src/store/userSlicec.ts
+++
@@ -1,4 +0,0 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import type { RootState } from "./store";
-import { login } from "../api/user";
-
