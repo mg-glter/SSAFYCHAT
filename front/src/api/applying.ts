769f30bbe590d084fa7f0e9e50a343582275b057
socm9145 <socm9145@naver.com>
2023-02-08 10:49:34 +0900

    [Feat] store, api : search 기능 추가

--- front/src/api/applying.ts
+++ front/src/api/applying.ts
@@ -2,11 +2,18 @@ import { localServer } from "./http";
 
 const api = localServer();
 
-async function search(mentoring:any) {
-    await api.post(`/mentoring/job/${mentoring.belong}/${mentoring.job}`)
+async function search(mentoring:any, success:any, fail:any) {
+    api.defaults.headers["Authorization"] = "Bearer " + sessionStorage.getItem("refresh-token");
+    await api.get(`/mentoring/search?job=${mentoring.job}&company=${mentoring.company}`)
+    .then(success)
+    .catch(fail);
+}
+
+async function apply(applying:any) {
+    await api.post(`/mentoring/apply`, JSON.stringify(applying))
     .then((data)=>{console.log(data);})
     .catch((err)=>{console.log(err);
     });
 }
 
-export {search};
+export {search, apply};
