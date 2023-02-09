da75efb14275b62f2cdec5b9fbb1b39a67ec7fe8
kdw3238 <kdw3238@naver.com>
2023-02-09 17:50:32 +0900

    [Fix] RollingPaper : 드래그 앤 드랍 기능 수정 중

--- front/src/components/rollingpaper/rollingpaper.tsx
+++ front/src/components/rollingpaper/rollingpaper.tsx
@@ -1,10 +1,15 @@
 import "../../styles/components/rollingpaper/rollingpaper.css";
-import Sticky from "../../widget/Sticky";
+// import Sticky from "../../widget/Sticky";
+import RollingCardList from "./RollingCardList";
+import { useState } from "react";
+
+
 
 function RollingPaper() {
     const imgUrlStar = "/img/Star.png"
     const imgUrlclean = "/img/clean.png"
     const imgUrlsave = "/img/save.png"
+    const[clickBtn, setClickBtn] = useState(false);
     return (
         <div className="rcontainer">
             <div className="dashboard">
@@ -20,23 +25,29 @@ function RollingPaper() {
                             <img className="clean_name" src={imgUrlclean} alt="Clean" />
                         </div>
                         <div className="save" onClick={()=>{
-                            
+                            console.log("리스트 나와라");
+                            setClickBtn(!clickBtn);
                         }}>
                             <img className="save_img" src={imgUrlsave} alt="Save" />
                         </div>
                     </div>
                 </div>
                 <div className="dashboard_main">
+                {/* 버튼을 클릭하면 */}
+                {clickBtn && (
+                    <RollingCardList closeList={()=>setClickBtn(false)}></RollingCardList>
+                )}  
                     {/* 롤링페이퍼 컨텐츠가 올 공간 입니다. */}
                 </div>
             </div>
             {/* <div className="sticky_list_container">
                 <div className="upper_arrow"></div>
                 <div>
-                    <Sticky title="감사합니다." text="덕분에 도움이 많이 됐어요"></Sticky>
+                <Sticky title="감사합니다." text="덕분에 도움이 많이 됐어요"></Sticky>
                 </div>
                 <div className="lower_arrow"></div>
             </div> */}
+            
         </div>
     )
 }
--- front/src/widget/Sticky.tsx
+++ front/src/widget/Sticky.tsx
@@ -4,15 +4,14 @@ function Sticky(props: any){
     return(
         // event : any, elemClassName : string, containerClassName : string, isEnterCheck : boolean
         <div className="sticky_main" onMouseDown={
-            (event:any)=>{dragRoll(event,"sticky_main","dashboard_main",true)}
+            (event:any)=>{dragRoll(event,"sticky_main","dashboard_main",true,props.closeList)}
         }>
             {/* <div className="title">
                 {props.title}
                 조원재
             </div> */}
             <div className="text">
-                {/* {props.text} */}
-                {/* 길지 않은 멘토링 시간 동안, 좋은 정보들을 제공해주신 멘토님 감사드립니다. 이번 채용 공고에 지원해서 꼭 같은 부서에서 만날 수 있었으면 좋겠습니다. 연락드리겠습니다! */}
+                {props.text}
             </div>
         </div>
     )
