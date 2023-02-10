1ee72ad5c6983a1264302f1501512d6d3dc548d5
kdw3238 <kdw3238@naver.com>
2023-02-10 12:49:33 +0900

    [Feat] RollingSlice : 롤링페이퍼에 사용할 슬라이스 생성

--- front/src/components/rollingpaper/RollingCardList.tsx
+++ front/src/components/rollingpaper/RollingCardList.tsx
@@ -1,14 +1,14 @@
 import "../../styles/components/rollingpaper/rolling-card-list.css";
 import Sticky from "../../widget/Sticky";
 function RollingCardList(props : any){
+    
+    const list = props.list;
+
     return(
         <div className="rolling_card_list">
             <div className="rolling_card_list_container">
                 <div className="rolling_card_list_inner_container">
-                    <Sticky closeList={props.closeList} text="길지 않은 멘토링 시간 동안, 좋은 정보들을 제공해주신 멘토님 감사드립니다. 이번 채용 공고에 지원해서 꼭 같은 부서에서 만날 수 있었으면 좋겠습니다. 연락드리겠습니다!"></Sticky>
-                    <Sticky closeList={props.closeList} text="길지 않은 멘토링 시간 동안, 좋은 정보들을 제공해주신 멘토님 감사드립니다. 이번 채용 공고에 지원해서 꼭 같은 부서에서 만날 수 있었으면 좋겠습니다. 연락드리겠습니다!"></Sticky>
-                    <Sticky closeList={props.closeList} text="길지 않은 멘토링 시간 동안, 좋은 정보들을 제공해주신 멘토님 감사드립니다. 이번 채용 공고에 지원해서 꼭 같은 부서에서 만날 수 있었으면 좋겠습니다. 연락드리겠습니다!"></Sticky>
-                    <Sticky closeList={props.closeList} text="길지 않은 멘토링 시간 동안, 좋은 정보들을 제공해주신 멘토님 감사드립니다. 이번 채용 공고에 지원해서 꼭 같은 부서에서 만날 수 있었으면 좋겠습니다. 연락드리겠습니다!"></Sticky>                  
+                    {list}
                 </div>
             </div>
         </div>
--- front/src/widget/Sticky.tsx
+++ front/src/widget/Sticky.tsx
@@ -1,17 +1,20 @@
 import "../styles/widget/sticky-style.css"
 import { dragRoll } from "../utils/ts/move";
 function Sticky(props: any){
+    
     return(
         // event : any, elemClassName : string, containerClassName : string, isEnterCheck : boolean
-        <div className="sticky_main" onMouseDown={
-            (event:any)=>{dragRoll(event,"sticky_main","dashboard_main",true,props.closeList)}
+        <div className="sticky_main" onClick={()=>{console.log(456)}} onMouseDown={
+            (event:any)=>{console.log(123);dragRoll(event,"sticky_main","dashboard_main",true,props.closeList)}
         }>
+            <div className={props.colorClass}>
             {/* <div className="title">
                 {props.title}
                 조원재
             </div> */}
-            <div className="text">
-                {props.text}
+                <div className="text">
+                    {props.text}
+                </div>
             </div>
         </div>
     )
