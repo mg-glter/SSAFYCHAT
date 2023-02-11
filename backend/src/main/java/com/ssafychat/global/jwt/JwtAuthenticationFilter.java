f722d5d2cd2975dfce43bc3c3c3f1412e5c0bdb4
sairo <from415@kakao.com>
2023-02-11 10:53:46 +0900

    [Refactor] JWT : JwtAuthenticationFilter 부모 클래스 변경

--- backend/src/main/java/com/ssafychat/global/jwt/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/ssafychat/global/jwt/JwtAuthenticationFilter.java
@@ -1,6 +1,5 @@
 package com.ssafychat.global.jwt;
 
-import com.ssafychat.domain.member.model.Member;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -8,19 +7,18 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
 @Slf4j
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter  extends GenericFilterBean {
+public class JwtAuthenticationFilter  extends OncePerRequestFilter {
     private static final String AUTHORIZATION_HEADER = "Authorization";
     private static final String BEARER_TYPE = "Bearer";
 
@@ -28,10 +26,10 @@ public class JwtAuthenticationFilter  extends GenericFilterBean {
     private final RedisTemplate redisTemplate;
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
 
         // 1. Request Header 에서 JWT 토큰 추출
-        String token = resolveToken((HttpServletRequest) request);
+        String token = resolveToken(request);
 
         // 2. validateToken 으로 토큰 유효성 검사
         if (token != null && jwtTokenProvider.validateToken(token)) {
@@ -41,7 +39,7 @@ public class JwtAuthenticationFilter  extends GenericFilterBean {
                 // 토큰이 유효할 경우 토큰에서 Authentication 객체를 가지고 와서 SecurityContext 에 저장
                 Authentication authentication = jwtTokenProvider.getAuthentication(token);
                 SecurityContextHolder.getContext().setAuthentication(authentication);
-                request.setAttribute("USER",(Member)authentication.getPrincipal());
+                request.setAttribute("USER", authentication.getPrincipal());
             }
         }
         chain.doFilter(request, response);
