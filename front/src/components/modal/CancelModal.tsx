5ab698093020c0602b49f29d4c85781154c80eb5
kdw3238 <kdw3238@naver.com>
2023-02-13 13:29:28 +0900

    [Feat] CancelModal : 멘티입장 취소기능 추가

--- front/src/components/modal/CancelModal.tsx
+++ front/src/components/modal/CancelModal.tsx
@@ -1,11 +1,12 @@
 import '../../styles/components/modal/cancel-modal.css';
-
+import { cancelMenteeMentoring } from '../../api/mentoring';
+import { useState } from 'react';
 function CancelModal (props:any){
-
+    let [text,setText] = useState("");
     function closeModal() {
         props.closeModal();
     }
-
+    let mId : number= props.info.mentoringId;
     return (
         <div className="cancel_modal" onClick={closeModal}>
             <div className="cancel_modal_body" onClick={(e) => e.stopPropagation()}>
@@ -16,12 +17,19 @@ function CancelModal (props:any){
                     </button>
                 </div>
                 <form className='cancel_modal_content'>
-                    <textarea className='cancel_modal_textarea' placeholder='취소사유를 적어주세요'>
+                    <textarea className='cancel_modal_textarea' placeholder='취소사유를 적어주세요' onChange={(event)=>{
+                        setText(event.target.value);
+                    }}>
                         
                     </textarea>
                     <div className='cancel_modal_submit_button_container'>                          
-                        <div className='cancel_modal_submit_button' onClick={
-                            closeModal
+                        <div className='cancel_modal_submit_button' onClick={()=>{
+                            cancelMenteeMentoring({mentoringId:mId,reason:text},(success : any)=>{
+                                closeModal();
+                                
+                            },(fail : any)=>{
+
+                            })}
                         }>제출</div>
                     </div>
                 </form>
