cb92e1959de019d432f3874602df2ae231d002b9
kdw3238 <kdw3238@naver.com>
2023-02-13 13:14:20 +0900

    [Feat] ReservationCard : 신청 받기 api

--- front/src/api/mentoring.ts
+++ front/src/api/mentoring.ts
@@ -45,7 +45,7 @@ async function getAppointmentApi(success : any, fail : any){
     .catch(fail);
 }
 // 요청멘토링 중 하나를 승낙한다
-async function setAppointment(appointment:{applyMentoringId:number, time:Date,}, success: any, fail: any){
+async function setAppointment(appointment:{applyMentoringId:number}, success: any, fail: any){
     api.defaults.headers["Authorization"] = "Bearer " + sessionStorage.getItem("access-token");
     await api.post(`/mentoring/appointment`,JSON.stringify(appointment))
     .then(success)
--- front/src/widget/ReservationCard.tsx
+++ front/src/widget/ReservationCard.tsx
@@ -1,7 +1,8 @@
 import "../styles/widget/reservation-card.css"
 import { useAppDispatch,useAppSelector } from "../hooks/hooks";
-import { getReservation } from "../store/mentoringSlice";
+import { getAppointment, getReservation } from "../store/mentoringSlice";
 import { cancelReservation } from "../api/mentoring";
+import { setAppointment } from "../api/mentoring";
 function whatMyNick(nick: number){
     console.log(nick);
     switch (nick){
@@ -30,6 +31,14 @@ function whatMyNick(nick: number){
 
 function ReservationCard(props : any){
 
+    // interface matchInfo{
+    //     name:string,
+    //     studentNumber:string,
+    //     numberth:number,
+    //     email:string,
+    //     time:string,
+    //     mentoringId:number,
+    //   }
     const dispatch = useAppDispatch();
     let funcAfterDrag : any;
     const reservationList = useAppSelector(state => state.mentoring.reservationList);
@@ -38,6 +47,21 @@ function ReservationCard(props : any){
         funcAfterDrag = ()=>{
             for(let i = 0; i < appointmentList.applys.length; ++i){
                 if(appointmentList.applys[i].applyMentoringId === props.info[4]){
+                    setAppointment(props.info[4],(success : any)=>{
+                        console.log(success);
+                        appointmentList.matches.push({
+                            name:appointmentList.applys[i].name,
+                            studentNumber:appointmentList.applys[i].studentNumber,
+                            numberth: appointmentList.applys[i].numberth,
+                            email: appointmentList.applys[i].email,
+                            time: appointmentList.applys[i].times[0],
+                            mentoringId: appointmentList.applys[i].applyMentoringId,
+                        });
+                        appointmentList.applys.splice(i,1);
+                        dispatch(getAppointment(appointmentList));
+                    },(fail : any)=>{
+                        console.log(fail);
+                    });
                     // 이곳에 api호출
                 }
             }
