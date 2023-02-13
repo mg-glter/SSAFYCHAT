558bdeceb93d3ab6ca243f0ac529718462384fcb
socm9145 <socm9145@naver.com>
2023-02-13 09:39:36 +0900

    [Fix] 메인 : CardBanner couterSlice 삭제

--- front/src/components/common/CardBanner.tsx
+++ front/src/components/common/CardBanner.tsx
@@ -1,19 +1,15 @@
 import "../../styles/components/common/card-banner.css"
-import { useAppSelector, useAppDispatch } from '../../hooks/hooks'
-import { decrement, increment } from '../../store/counterSlice'
 function CardBanner (){
-    const count = useAppSelector(state => state.counter.value)
-    const dispatch = useAppDispatch()
     return (
         <div className="card_banner" >
             <div className="cell_a"></div>
             <div className="cell_b"></div>
             <div className="cell_c">
                 <div className="card_banner_content">
-                    <p className="card_banner_header" onClick={()=>{dispatch(increment())}}>싸피챗이 무엇인가요? {count}</p>
+                    <p className="card_banner_header">싸피챗이 무엇인가요?</p>
                     <p className="card_banner_main">싸피챗을 시작해 보세요!</p>
                     <p className="card_banner_main">당신의 커리어를 위해</p>
-                    <button className="card_banner_button" onClick={()=>{dispatch(decrement())}}>시작하기</button>
+                    <button className="card_banner_button">시작하기</button>
                 </div>
             </div>
             <div className="cell_d"></div>
